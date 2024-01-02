e1e6b8846d361b2133118cec6720da302e59c333
José Augusto <josemoura212@gmail.com>
2024-01-02 13:38:39 -0300

    Add Pessoa struct and related functions

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,46 @@ enum Coordenada {
     TresD(i32, i32, i32),
 }
 
+struct Pessoa {
+    nome: String,
+    idade: u8,
+    altura: f32,
+}
+
 fn main() {
     enumeracao(Fruta::Morango);
     enumeracao(Fruta::Banana);
     enumeracao(Fruta::Maca);
     enumeracao(Fruta::Acai);
 
+    enumeracao_com_dados();
+
+    estrutura();
+}
+
+fn estrutura() {
+    let glaucio = Pessoa {
+        nome: String::from("Glaucio"),
+        idade: 20,
+        altura: 1.75,
+    };
+
+    println!("\nNome: {}", glaucio.nome);
+    println!("Idade: {}", glaucio.idade);
+    println!("Altura: {}\n", glaucio.altura);
+}
+
+fn enumeracao_com_dados() {
     let ponto2d = Coordenada::DoisD(5, 10);
     let ponto3d = Coordenada::TresD(5, 10, 15);
 
     match ponto2d {
-        Coordenada::DoisD(x, y) => println!("Coordenada 2D: x = {}, y = {}", x, y),
+        Coordenada::DoisD(x, y) => println!("\nCoordenada 2D: x = {}, y = {}", x, y),
         Coordenada::TresD(_, _, _) => println!("Coordenada 3D"),
     }
     match ponto3d {
         Coordenada::DoisD(_, _) => println!("Coordenada 2D"),
-        Coordenada::TresD(x, y, z) => println!("Coordenada 3D: x = {}, y = {}, z = {}", x, y, z),
+        Coordenada::TresD(x, y, z) => println!("\nCoordenada 3D: x = {}, y = {}, z = {}", x, y, z),
     }
 }
 
